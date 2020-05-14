82a5a4a3b8359b57f50f7360a45886855263d7f7
margauxsvrn <margauxsouvairan@gmail.com>
2020-05-14 12:01:48 +0200

    Formulaire

---
+++ form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Emma spécialiste cynothérapie en EHPAD ou auprès de particulier.">
+    <link rel="stylesheet" href="style.css">
+    <!--Police-->
+    <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/655edc4fae.js" crossorigin="anonymous"></script>
+
+    <link rel="shortcut icon" type="image/x-icon" href="pic/logo-emma.ico" />
+
+    <title>Emma - Cynothérapie</title>
+</head>
+
+<center>
+
+<?php
+
+echo '<p>Merci <strong>' . $_POST['nom'] . '</strong> !</p><p>Un peu de patience, le site étant toujours en maintenance.</p> <p style="color:#C79E7C">Emma.</p>';
+
+?>
+<br>
+<p style="font-size:16px">Pour retourner sur le site, <button style="color:#C79E7C"><a href="index.html"> cliquez ici</a></button></p>
+
+</center>
